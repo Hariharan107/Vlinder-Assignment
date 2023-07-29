a4f20fdc0957586b1d62545806534c5fbfc2a538
Hariharan <hariharen107@gmail.com>
2023-07-29 20:41:08 +0530

    Added carousel autoplay and refactored code

--- src/components/Sections/Gallery.tsx
+++ src/components/Sections/Gallery.tsx
@@ -1,17 +1,37 @@
+import { useMediaQuery } from "react-responsive";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
-import { EffectCoverflow, Pagination } from "swiper/modules";
-import styles from "./Gallery.module.css"; // Import the CSS Module
+import "swiper/css/autoplay";
+import { EffectCoverflow, Pagination, Autoplay } from "swiper/modules";
+import styles from "./Gallery.module.css";
+import { ImagesData } from "../../data/CarouselData";
+
+interface CarouselImageProps {
+  id: number;
+  imageSrc: string;
+  altText: string;
+}
 
 export default function Gallery() {
+  const isLaptop = useMediaQuery({ minWidth: 768 });
+  const centerSlides = Boolean(!isLaptop);
+  const CarouselImages = ImagesData.map(
+    ({ id, imageSrc, altText }: CarouselImageProps) => (
+      <SwiperSlide key={id} className={styles["swiper-slide"]}>
+        <img src={imageSrc} alt={altText} className={styles.img} />
+      </SwiperSlide>
+    )
+  );
+  true;
+
   return (
-    <>
+    <div className='pb-5'>
       <Swiper
         effect={"coverflow"}
         grabCursor={true}
-        centeredSlides={true}
+        centeredSlides={centerSlides}
         slidesPerView={"auto"}
         coverflowEffect={{
           rotate: 50,
@@ -21,64 +41,17 @@ export default function Gallery() {
           slideShadows: true,
         }}
         pagination={true}
+        autoplay={{
+          disableOnInteraction: false,
+          delay: 2000,
+          pauseOnMouseEnter: true,
+        }}
+        loop={true}
         className={styles.swiper}
-        modules={[EffectCoverflow, Pagination]}
+        modules={[EffectCoverflow, Pagination, Autoplay]}
       >
-        <SwiperSlide className={styles["swiper-slide"]}>
-          <img
-            src='https://swiperjs.com/demos/images/nature-1.jpg'
-            className={styles.img}
-          />
-        </SwiperSlide>
-        <SwiperSlide className={styles["swiper-slide"]}>
-          <img
-            src='https://swiperjs.com/demos/images/nature-2.jpg'
-            className={styles.img}
-          />
-        </SwiperSlide>
-        <SwiperSlide className={styles["swiper-slide"]}>
-          <img
-            src='https://swiperjs.com/demos/images/nature-3.jpg'
-            className={styles.img}
-          />
-        </SwiperSlide>
-        <SwiperSlide className={styles["swiper-slide"]}>
-          <img
-            src='https://swiperjs.com/demos/images/nature-4.jpg'
-            className={styles.img}
-          />
-        </SwiperSlide>
-        <SwiperSlide className={styles["swiper-slide"]}>
-          <img
-            src='https://swiperjs.com/demos/images/nature-5.jpg'
-            className={styles.img}
-          />
-        </SwiperSlide>
-        <SwiperSlide className={styles["swiper-slide"]}>
-          <img
-            src='https://swiperjs.com/demos/images/nature-6.jpg'
-            className={styles.img}
-          />
-        </SwiperSlide>
-        <SwiperSlide className={styles["swiper-slide"]}>
-          <img
-            src='https://swiperjs.com/demos/images/nature-7.jpg'
-            className={styles.img}
-          />
-        </SwiperSlide>
-        <SwiperSlide className={styles["swiper-slide"]}>
-          <img
-            src='https://swiperjs.com/demos/images/nature-8.jpg'
-            className={styles.img}
-          />
-        </SwiperSlide>
-        <SwiperSlide className={styles["swiper-slide"]}>
-          <img
-            src='https://swiperjs.com/demos/images/nature-8.jpg'
-            className={styles.img}
-          />
-        </SwiperSlide>
+        {CarouselImages}
       </Swiper>
-    </>
+    </div>
   );
 }
---
+++ src/data/CarouselData.js
@@ -0,0 +1,123 @@
+export const ImagesData = [
+  {
+    id: 1,
+    imageSrc:
+      "https://images.pexels.com/photos/3246665/pexels-photo-3246665.png?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 1",
+  },
+  {
+    id: 2,
+    imageSrc:
+      "https://images.pexels.com/photos/587958/pexels-photo-587958.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 2",
+  },
+  {
+    id: 3,
+    imageSrc:
+      "https://images.pexels.com/photos/2372982/pexels-photo-2372982.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 3",
+  },
+  {
+    id: 4,
+    imageSrc:
+      "https://images.pexels.com/photos/3893650/pexels-photo-3893650.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 4",
+  },
+  {
+    id: 5,
+    imageSrc:
+      "https://images.pexels.com/photos/3660035/pexels-photo-3660035.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 5",
+  },
+  {
+    id: 6,
+    imageSrc:
+      "https://images.pexels.com/photos/1891882/pexels-photo-1891882.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 6",
+  },
+  {
+    id: 7,
+    imageSrc:
+      "https://images.pexels.com/photos/11942721/pexels-photo-11942721.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 7",
+  },
+  {
+    id: 8,
+    imageSrc:
+      "https://images.pexels.com/photos/885880/pexels-photo-885880.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 8",
+  },
+  {
+    id: 9,
+    imageSrc:
+      "https://images.pexels.com/photos/1271620/pexels-photo-1271620.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 9",
+  },
+  {
+    id: 10,
+    imageSrc:
+      "https://images.pexels.com/photos/2918010/pexels-photo-2918010.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 10",
+  },
+  // Add 10 more data objects here
+  {
+    id: 11,
+    imageSrc:
+      "https://images.pexels.com/photos/3250356/pexels-photo-3250356.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 11",
+  },
+  {
+    id: 12,
+    imageSrc:
+      "https://images.pexels.com/photos/1840421/pexels-photo-1840421.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 12",
+  },
+  {
+    id: 13,
+    imageSrc:
+      "https://images.pexels.com/photos/732632/pexels-photo-732632.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 13",
+  },
+  {
+    id: 14,
+    imageSrc:
+      "https://images.pexels.com/photos/1194233/pexels-photo-1194233.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 14",
+  },
+  {
+    id: 15,
+    imageSrc:
+      "https://images.pexels.com/photos/1392099/pexels-photo-1392099.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 15",
+  },
+  {
+    id: 16,
+    imageSrc:
+      "https://images.pexels.com/photos/775204/pexels-photo-775204.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 16",
+  },
+  {
+    id: 17,
+    imageSrc:
+      "https://images.pexels.com/photos/1822463/pexels-photo-1822463.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 17",
+  },
+  {
+    id: 18,
+    imageSrc:
+      "https://images.pexels.com/photos/1918290/pexels-photo-1918290.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 18",
+  },
+  {
+    id: 19,
+    imageSrc:
+      "https://images.pexels.com/photos/699466/pexels-photo-699466.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 19",
+  },
+  {
+    id: 20,
+    imageSrc:
+      "https://images.pexels.com/photos/15032623/pexels-photo-15032623/free-photo-of-painting-flowers-on-wall.jpeg?auto=compress&cs=tinysrgb&w=600",
+    altText: "Image 20",
+  },
+];
